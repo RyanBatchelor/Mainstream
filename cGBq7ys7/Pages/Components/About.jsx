3315a2912dda858f8e133a4cfde403dd3161c9ab
Ryan Batchelor <121133330+RyanBatchelor@users.noreply.github.com>
2023-06-16 10:14:38 -0500

    Add files via upload

---
+++ cGBq7ys7/Pages/Components/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <section id="about">
+        <h2>About Mainstream Bar and Grill</h2>
+        <div className="about-columns">
+            <div className="about-column">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id nibh sit amet est gravida congue ut vitae urna. Vestibulum dapibus facilisis lectus eu porttitor.</p>
+            </div>
+            <div className="about-column">
+                <img src="../../images/burger.jpg" alt="Placeholder Image"/>
+            </div>
+        </div>
+    </section>
+  );
+}
+
+export default About
