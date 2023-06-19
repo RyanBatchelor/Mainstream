12f449eead995966e7cf10b3030e92a5b18dc4c6
Ryan Batchelor <121133330+RyanBatchelor@users.noreply.github.com>
2023-06-19 12:33:34 -0500

    Add files via upload

---
+++ Pages/Components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section id="home">
+    <div className="hero-content">
+      <h1>Welcome to Mainstream Bar and Grill</h1>
+      <p>We offer the best food and drinks in the city. Come and join us for an unforgettable dining experience.</p>
+      <button>Reserve a Table</button>
+    </div>
+    <div className="hero-image">
+      <img src="././images/storefront.jpg" alt="Restaurant Image"/>
+    </div>
+  </section>
+  );
+}
+
+export default Hero
